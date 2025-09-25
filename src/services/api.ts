cd7b35c9c1d70c6722ae141f4cc663040a9c0c2d
DanielMb <Dapierre25@gmail.com>
2025-09-26 00:39:29 +0100

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'backend-production-121a.up.railway.app';
 
 export interface ApiResponse<T> {
   success: boolean;
